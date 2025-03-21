59cc27add672e4895df3b07bd1532fbcf4bbd35c
Enrique <henrirodnav@gmail.com>
2025-03-21 12:53:36 +0100

    what's 9 + 10?

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hola mundo");
+sol = 21
+
+function whats9plus10(sol){
+    switch (sol) {
+        case 19:
+            console.log("19 dollar fortnite card, who wants it?");
+            break;
+    
+        default:
+            console.log("you stupid")
+            break;
+    }
+}
